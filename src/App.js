7f9cc24f227fa301f7e45b564e9785f21236cf15
Zlira <olesia.hr@gmail.com>
2019-02-08 21:41:54 +0200

    Added text for chapter one

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import CaesarCipherWidget from './components/CaesarWidget';
-import CaesarBreakWidget from './components/CaesarBreakWidget'
-import { caesarText } from './content/cipherTexts'
-import { encipher } from './ciphers/caesar'
+import ChapterOne from './content/ChapterOne'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CaesarCipherWidget
-          title='Метод Цезаря: Шифрування'
-          text='прочитав твою нову книжку'
-        />
-        <CaesarCipherWidget
-          title='Метод Цезаря: Розшифрування'
-          text='тусьйхгд хдсб рсдц нрйінц'
-          reverse
-        />
-        <CaesarBreakWidget
-          title='Метод Цезаря: Злом'
-          text={encipher(19, caesarText.toLowerCase())}
-        />
+        <ChapterOne />
       </div>
     );
   }
