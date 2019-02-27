7025c3a157ee2a7fa8cc4e9ef3fb510914d8b199
Zlira <olesia.hr@gmail.com>
2019-02-27 15:01:42 +0200

    Added reference text letter frequency

--- src/components/MonoalphabetBreakWidget.js
+++ src/components/MonoalphabetBreakWidget.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { monoAlphBreakText } from '../content/letterFrequency'
+import { monoAlphBreakText, Chapter2Text } from '../content/letterFrequency'
 import './MonoalphabetBreakWidget.css'
 
 
@@ -10,6 +10,7 @@ export default function MonoalphabetBreakWidget({text}) {
       <h3 className='cipher-widget__title'>Метод одноалфавітної заміни: Злом</h3>
       <div className='cipher-widget__body'>
         <LetterFreques lettersInfo={monoAlphBreakText}/>
+        <LetterFreques lettersInfo={Chapter2Text} upsidedown/>
         <p className="cipher-widget__secret-text cipher-widget__text ">
           {text}
         </p>
@@ -19,38 +20,42 @@ export default function MonoalphabetBreakWidget({text}) {
 }
 
 
-function LetterFreques({lettersInfo}) {
+function LetterFreques({lettersInfo, upsidedown}) {
   const letters = []
   for (let i=0; i < Object.keys(lettersInfo).length; i++) {
     let letter = lettersInfo[i]
     letters.push(
-      <LetterFreq letter={letter.letter} freq={letter.freq} key={letter.letter} />
+      <LetterFreq letter={letter.letter} freq={letter.freq} upsidedown={upsidedown}
+        key={letter.letter} />
     )
   }
   return <div>{letters}</div>
 }
 
-function LetterFreq({letter, freq}) {
+function LetterFreq({letter, freq, upsidedown}) {
+  const freqInd = <FreqIndicator freq={freq} upsidedown={upsidedown} />
   return <div className="cipher-widget__text cipher-widget__letter-freq">
-    <FreqIndicator freq={freq} />
-    <div >{letter}</div>
+    {upsidedown? null : freqInd}
+    <div style={{width: "100%", textAlign: "center"}}>{letter}</div>
+    {upsidedown? freqInd : null}
   </div>
 }
 
-function FreqIndicator({freq}) {
+function FreqIndicator({freq, upsidedown}) {
   const width = 14, height=42
   const freqR = Math.round(freq)
   const bars = []
   for (let i=0; i < freqR; i++) {
     bars.push(
-      <line key={i} x1={0} x2={width} y1={i * 4} y2={i * 4}
+      <line key={i} x1={0} x2={width} y1={i * 4 + 1} y2={i * 4 + 1}
         stroke="#00EB5F" strokeWidth={2} />
     )
   }
+  const transform = upsidedown? 'translate(0, 3)' : `translate(${width}, ${height}) rotate(-180)`
   return (
     <svg width={width} height={height}>
       <title>{freq.toFixed(2) + '%'}</title>
-      <g transform={`translate(${width}, ${height}) rotate(-180)`}>{bars}</g>
+      <g transform={transform}>{bars}</g>
     </svg>
   )
 }
